5385b381311f8678e18e1116aa1cd87da0f48303
IMS-Global <brennick.sci@gmail.com>
2017-07-06 06:47:30 -0600

    Updated Favorites and Addresses Schema for Database Tables

--- db/migrate/20170705222727_create_addresses.rb
+++ db/migrate/20170705222727_create_addresses.rb
@@ -2,11 +2,11 @@ class CreateAddresses < ActiveRecord::Migration[5.1]
   def change
     create_table :addresses do |t|
       t.favorite :belongs_to
-      t.string :street1
+      t.string :street1, null: false
       t.string :street2
-      t.string :city
-      t.string :state
-      t.string :zipcode
+      t.string :city, null: false
+      t.string :state, null: false
+      t.string :zipcode, null: false
 
       t.timestamps
     end
