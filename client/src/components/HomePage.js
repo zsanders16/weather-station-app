c2bf5081af15a4fc48066b010309fefb24cc7884
Aaron Graupman <aaron.emmi3@gmail.com>
2017-07-08 14:01:48 -0600

    add todays weather

--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import TodaysWeather from './TodaysWeather'
 
 class HomePage extends Component {
   render() {
     return(
       <h1>
         HomePage
+        <TodaysWeather/>
       </h1>
     )
   }
---
+++ client/src/components/TodaysWeather.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class TodaysWeather extends Component {
+  render() {
+    return(
+      <h1>
+        {"These are today's current weather conditions."}
+      </h1>
+    )
+  }
+}
+
+export default TodaysWeather
