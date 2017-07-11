d0b442552700228b47d2a7aa01c1758cea4980db
IMS-Global <brennick.sci@gmail.com>
2017-07-11 07:56:59 -0600

    Actual Sensor data overlay on historical data

--- client/src/components/SensorActual.js
+++ client/src/components/SensorActual.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import { Grid, Segment, Form, Checkbox } from 'semantic-ui-react'
+import { Grid, Segment, Form, Checkbox, Divider, Header } from 'semantic-ui-react'
 import { sensorActual, sensorHistorical, sensorReset } from '../actions/sensor'
 import ReactHighcharts from 'react-highcharts'
 import Datetime from 'react-datetime'
@@ -12,6 +12,38 @@ import 'react-datetime/css/react-datetime.css'
 class SensorActual extends Component {
   // Tracker for chart display options selected by the user
   state = {
+    settings: {
+      actual: {
+        tempViews: {
+          celsius: true,
+          fahrenheit: false,
+          kelvin: false,
+        },
+        display: {
+          callback: sensorActual,
+          state: true,
+        },
+        start_date: moment().subtract(2, 'day'),
+        end_date: moment(),
+        limit: 30,
+      },
+      historical: {
+        tempViews: {
+          celsius: true,
+          fahrenheit: false,
+          kelvin: false,
+        },
+        display: {
+          callback: sensorHistorical,
+          state: true,
+        },
+        start_date: moment().subtract(2, 'day'),
+        end_date: moment(),
+        limit: 30,
+      },
+    },
+
+    // TODO: Remove these settings after seperating the graph types
     views: {
       celsius: true,
       fahrenheit: false,
@@ -47,198 +79,254 @@ class SensorActual extends Component {
   }
 
   setHistoricalChartType = () => {
-    let { sensor: { historical }, dispatch } = this.props
-    let { chart_type, start_date, end_date } = this.state
-    // Collect any required data from remote database
-    // if( !historical || historical.length <= 0 ){
-    //   // Check if activated
-      if( chart_type.historical.state ) {
-        dispatch(chart_type.historical.callback({
-          start_date: start_date.format(this.postgresql),
-          end_date: end_date.format(this.postgresql),
-        }))
-      }
-    // }
+    let { dispatch } = this.props
+    let { display, start_date, end_date } = this.state.settings.historical
+    if( display.state ) {
+      dispatch(display.callback({
+        start_date: start_date.format(this.postgresql),
+        end_date: end_date.format(this.postgresql),
+      }))
+    }
   }
 
   setActualChartType = () => {
-    let { sensor: { actual }, dispatch } = this.props
-    let { chart_type, start_date, end_date } = this.state
-    // if( !actual || actual.length <= 0 ){
-      if( chart_type.actual.state ) {
-        dispatch(chart_type.actual.callback({
-          start_date: start_date.format(this.postgresql),
-          end_date: end_date.format(this.postgresql),
-        }))
-      }
-    // }
+    let { dispatch } = this.props
+    let { display, start_date, end_date } = this.state.settings.actual
+    if( display.state ) {
+      dispatch(display.callback({
+        start_date: start_date.format(this.postgresql),
+        end_date: end_date.format(this.postgresql),
+      }))
+    }
   }
 
 
   setChartData = () => {
     let { sensor: { actual, historical } } = this.props
-    let { chart_type } = this.state
+    let { actual: act, historical: hist } = this.state.settings
+
+    // clear the existing series data
     this.state.series = []
-    if( chart_type.historical.state ) {
+
+    if( hist.display.state ) {
       if( historical && historical.length > 0 ) {
-        this.setDataSeries(historical, 'Hist.')
+        this.setDataSeries(historical, hist, 'Hist.')
       }
     }
-    if( chart_type.actual.state ) {
+    if( act.display.state ) {
       if( actual && actual.length > 0) {
-        this.setDataSeries(actual, 'Act.')
+        this.setDataSeries(actual, act, 'Act.')
       }
     }
   }
 
   // historical is an array of data objects
-  setDataSeries = ( data, type = '' ) => {
-    let { series, views, chart_type } = this.state
-    let { categories } = this.state.xAxis
+  setDataSeries = ( data, settings, type = '' ) => {
+    let { series } = this.state
     if( data && data.length > 0 ) {
       // Grab each view, i.e. F,C,K
-      for( let view in views ){
-        if( views[view] ) {
-          switch(view) {
-            case 'celsius':
-              series.push({
-                name: type + ' - Celsius',
-                data: this.parseTempData( data, 'celsius', categories ),
-              })
-              break;
-            case 'fahrenheit':
-              series.push({
-                name: type + ' - Fahrenheit',
-                data: this.parseTempData( data, 'fahrenheit', categories ),
-              })
-              break;
-            case 'kelvin':
-              series.push({
-                name: type + ' - Kelvin',
-                data: this.parseTempData( data, 'kelvin', categories ),
-              })
-              break;
-            default:
-              series.push({
-                name: type + ' - Celsius',
-                data: this.parseTempData( data, 'celsius', categories ),
-              })
-          }
+      for( let view in settings.tempViews ){
+        if( settings.tempViews[view] ) {
+          series.push({
+            name: type + ' - ' + view.charAt().toUpperCase() + view.substr(1),
+            data: this.parseTempData( data, view ),
+          })
         }
       }
     }
   }
 
-  parseTempData = ( data, view, categories ) => {
+  parseTempData = ( data, view ) => {
     return data.map( ( data ) => {
       // TODO: Determine a set of categories that will work with all datasets types
       // categories.push( moment(data.created_at).format('mm:ss') )
-      return data[view]
+      let category = moment(data.created_at).format('mm:ss')
+      let point = data[view]
+      return [ category, point ]
     })
   }
 
-  viewChanged = (event,data) => {
+  /*
+   * NOTE: Controls for determining which data sets will be displayed
+   */
+
+   // Change the Temperature Views that are being displayed
+  actViewChanged = (event,data) => {
     let { id } = data
-    let viewSelection = this.state.views
-    viewSelection[id] = !viewSelection[id]
-    this.setState({ views: viewSelection })
+    let settings = this.state.settings
+    settings.actual.tempViews[id] = !settings.actual.tempViews[id]
+    this.setState({ settings })
+  }
+  histViewChanged = (event,data) => {
+    let { id } = data
+    let settings = this.state.settings
+    settings.historical.tempViews[id] = !settings.historical.tempViews[id]
+    this.setState({ settings })
   }
 
-  handleStartDate = ( moment ) => {
-    // TODO: Clear the sensor data if Not checked
+  // Change the Timestamps being used to query the data to display
+  handleActStartDate = ( moment ) => {
     let { dispatch } = this.props
-    this.setState({ start_date: moment })
+    let settings = this.state.settings
+    settings.historical.start_date = moment
+    this.setState({ settings })
+    dispatch(sensorReset())
+    this.setChartTypes()
+  }
+  handleHistStartDate = ( moment ) => {
+    let { dispatch } = this.props
+    let settings = this.state.settings
+    settings.historical.start_date = moment
+    this.setState({ settings })
     dispatch(sensorReset())
     this.setChartTypes()
   }
 
-  handleEndDate = ( moment ) => {
-    // TODO: Clear the sensor data if Not checked
+  handleActEndDate = ( moment ) => {
     let { dispatch } = this.props
-    this.setState({ end_date: moment })
+    let settings = this.state.settings
+    settings.actual.end_date = moment
+    this.setState({ settings })
+    dispatch(sensorReset())
+    this.setChartTypes()
+  }
+  handleHistEndDate = ( moment ) => {
+    let { dispatch } = this.props
+    let settings = this.state.settings
+    settings.historical.end_date = moment
+    this.setState({ settings })
     dispatch(sensorReset())
     this.setChartTypes()
   }
 
-  handleChartType = (event,data) => {
-    let { id, checked } = data
-    let chartType = this.state.chart_type
+  // Change the charts that will be displayed on the graph
+  handleActDisplay = (event,data) => {
+    let { checked } = data
 
-    chartType[id].state = !chartType[id].state
-    this.setState({ chart_type: chartType })
+    let settings = this.state.settings
+    settings.actual.display.state = !settings.actual.display.state
+    this.setState({ settings })
 
-    if( id === 'actual' && checked === true ){
+    if( checked === true ){
       this.setActualChartType()
-    } else if( id === 'historical' && checked === true ) {
+    }
+  }
+  handleHistDisplay = (event,data) => {
+    let { checked } = data
+
+    let settings = this.state.settings
+    settings.historical.display.state = !settings.historical.display.state
+    this.setState({ settings })
+
+    if( checked === true ) {
       this.setHistoricalChartType()
     }
   }
 
+  /*
+   * Rendering of the Chart and the Control/Settings panel
+   */
   render(){
     this.setChartData()
-    let { views, start_date, end_date, chart_type } = this.state
+    let { actual: act, historical: hist } = this.state.settings
     return (
-      <Segment>
-        <ReactHighcharts config={this.state} />
-        <Form>
-          <Grid>
-            <Grid.Row columns={2}>
-              <Grid.Column width={8}>
-                <Form.Field>
-                  <label>Chart Type</label>
-                  <Checkbox
-                    id='actual'
-                    label='Actual'
-                    checked={chart_type.actual.state}
-                    onChange={this.handleChartType} /> &nbsp;
+      <Segment basic compact>
+        <Segment>
+          <ReactHighcharts config={this.state} />
+        </Segment>
+        <Segment>
+          <Form>
+            <Grid>
+              <Grid.Row columns={2}>
+                <Grid.Column width={8}>
+                  <Header as='h4'>Actual Realtime Data</Header>
+                  <Divider />
+                  <Form.Field>
+                    <label>Display</label>
+                    <Checkbox
+                      id='actual'
+                      label='Actual'
+                      checked={act.display.state}
+                      onChange={this.handleActDisplay} /> &nbsp;
+                  </Form.Field>
+                  <Form.Field>
+                    <label>Temperatures</label> &nbsp;
+                    <Checkbox
+                      id='celsius'
+                      label='Celsius'
+                      checked={act.tempViews.celsius}
+                      onChange={this.actViewChanged} /> &nbsp;
+                    <Checkbox
+                      id='fahrenheit'
+                      label='Fahrenheit'
+                      checked={act.tempViews.fahrenheit}
+                      onChange={this.actViewChanged} /> &nbsp;
+                    <Checkbox
+                      id='kelvin'
+                      label='Kelvin'
+                      checked={act.tempViews.kelvin}
+                      onChange={this.actViewChanged} />
+                  </Form.Field>
+                  <Form.Field>
+                    <label>Start</label>
+                    <Datetime compact
+                      value={act.start_date}
+                      input={true}
+                      onChange={this.handleActStartDate} />
+                    <label>End Date</label>
+                    <Datetime compact
+                      value={act.end_date}
+                      input={true}
+                      onChange={this.handleActEndDate} />
+                  </Form.Field>
+                </Grid.Column>
+
+                <Grid.Column width={8}>
+                  <Header as='h4'>Historical Data</Header>
+                  <Divider />
+                  <Form.Field>
+                  <label>Display</label>
                   <Checkbox
                     id='historical'
                     label='Historical'
-                    checked={chart_type.historical.state}
-                    onChange={this.handleChartType} />
-                </Form.Field>
-              </Grid.Column>
-              <Grid.Column width={8}>
-                <Form.Field>
-                  <label>Temperatures</label> &nbsp;
-                  <Checkbox
-                    id='celsius'
-                    label='Celsius'
-                    checked={views.celsius}
-                    onChange={this.viewChanged} /> &nbsp;
-                  <Checkbox
-                    id='fahrenheit'
-                    label='Fahrenheit'
-                    checked={views.fahrenheit}
-                    onChange={this.viewChanged} /> &nbsp;
-                  <Checkbox
-                    id='kelvin'
-                    label='Kelvin'
-                    checked={views.kelvin}
-                    onChange={this.viewChanged} />
-                </Form.Field>
-              </Grid.Column>
-            </Grid.Row>
-            <Grid.Row columns={3}>
-              <Grid.Column width={6}></Grid.Column>
-              <Grid.Column width={4}>
-                <Form.Field>
-                  <label>Start</label>
-                  <Datetime compact
-                    value={start_date}
-                    input={true}
-                    onChange={this.handleStartDate} />
-                  <label>End Date</label>
-                  <Datetime compact
-                    value={end_date}
-                    input={true}
-                    onChange={this.handleEndDate} />
-                </Form.Field>
-              </Grid.Column>
-              <Grid.Column width={6}></Grid.Column>
-            </Grid.Row>
-          </Grid>
-        </Form>
+                    checked={hist.display.state}
+                    onChange={this.handleHistDisplay} />
+                  </Form.Field>
+                  <Form.Field>
+                    <label>Temperatures</label> &nbsp;
+                    <Checkbox
+                      id='celsius'
+                      label='Celsius'
+                      checked={hist.tempViews.celsius}
+                      onChange={this.histViewChanged} /> &nbsp;
+                    <Checkbox
+                      id='fahrenheit'
+                      label='Fahrenheit'
+                      checked={hist.tempViews.fahrenheit}
+                      onChange={this.histViewChanged} /> &nbsp;
+                    <Checkbox
+                      id='kelvin'
+                      label='Kelvin'
+                      checked={hist.tempViews.kelvin}
+                      onChange={this.histViewChanged} />
+                  </Form.Field>
+                  <Form.Field>
+                    <label>Start</label>
+                    <Datetime compact
+                      value={hist.start_date}
+                      input={true}
+                      onChange={this.handleHistStartDate} />
+                    <label>End Date</label>
+                    <Datetime compact
+                      value={hist.end_date}
+                      input={true}
+                      onChange={this.handleHistEndDate} />
+                  </Form.Field>
+                </Grid.Column>
+              </Grid.Row>
+            </Grid>
+          </Form>
+        </Segment>
       </Segment>
     )
   }
